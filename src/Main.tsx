d8e070b31fb02233a96ffbf116d7b060a5522467
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-09-06 06:42:32 +0100

    feature: added list types to main file

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,10 +2,20 @@ import React from 'react'
 import NoteInput from './components/NoteInput';
 
 function App(){
+    const [note, setNote] = React.useState([])
+
     return(
         <div>
             <NoteInput />
             <hr/>
+            <ul>
+                {
+                    note.map((singleNote) =>
+                        <li>{singleNote} </li>
+                    )
+                }
+
+            </ul>
         </div>
     )
 }
